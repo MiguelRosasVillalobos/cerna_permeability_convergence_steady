a8ac86a77056061138acd22008331a2fbddb81f6
miguelrosasvillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-07-22 00:40:21 -0400

    se corrige error de directorios

--- start_simulation.sh
+++ start_simulation.sh
@@ -9,15 +9,17 @@ fi
 # Obtiene la cantidad_simulaciones desde el primer argumento
 cantidad_simulaciones=$1
 
-lc1=0.02
-lc2=0.01
-lc3=0.009
-lc4=0.008
-lc5=0.007
-lc6=0.006
+lc1=0.01
+lc2=0.008
+lc3=0.007
+lc4=0.006
+lc5=0.005
+lc6=0.004
+lc7=0.003
+lc8=0.002
 
 # Valores de Reynolds a utilizar
-valores_lc=("lc1" "lc2" "lc3" "lc4" "lc5" "lc6")
+valores_lc=("lc1" "lc2" "lc3" "lc4" "lc5" "lc6" "lc7" "lc8")
 
 # Leer valores desde el archivo parametros.txt
 nu=$(grep -oP 'nu\s*=\s*\K[\d.+-]+' parametros.txt)
@@ -85,36 +87,36 @@ for ((i = 1; i <= $cantidad_simulaciones; i++)); do
 	mv mesh.geo Case_0/
 	mv mesh.msh Case_0/
 	mv deltap_extract.py Case_0/
+	cd "./Case_0/"
+	cd "./geometry_script/"
+	#Generar mallado gmsh
+	python3 generator_point_process.py
+	./generate.sh
+	cd ../..
 
-	# Se inicia el cilclo para variar el valor de Reynolds
+	# Se inicia el ciclo para variar el valor de lc
 	for j in {0..7}; do
 		#se genera contador k
 		k=$((j + 1))
 
 		# se crea carpeta del caso para el valor de Reynolds
-		mkdir Case_${i}_${valores_lc[$j]}
+		carpeta_lc="Case_${i}_${valores_lc[$j]}"
+		mkdir "$carpeta_lc"
 
-		#se copian los archivops a la carpeta del caso
-		cp -r Case_0/0/ Case_${i}_${valores_lc[$j]}/
-		cp -r Case_0/constant/ Case_${i}_${valores_lc[$j]}/
-		cp -r Case_0/system/ Case_${i}_${valores_lc[$j]}/
-		cp -r Case_0/geometry_script/ Case_${i}_${valores_lc[$j]}/
-		cp Case_0/mesh.geo Case_${i}_${valores_lc[$j]}/
+		#se copian los archivos a la carpeta del caso
+		cp -r Case_0/0/ "$carpeta_lc/"
+		cp -r Case_0/constant/ "$carpeta_lc/"
+		cp -r Case_0/system/ "$carpeta_lc/"
+		cp -r Case_0/geometry_script/ "$carpeta_lc/"
+		cp Case_0/mesh.geo "$carpeta_lc/"
 
 		#Se reemplaza el valor de lc en el archivo 0/U
-		sed -i "s/\$lccc/${!valores_lc[$j]}/g" Case_${i}_${valores_lc[$j]}/mesh.geo
-		sed -i "s/\$lccc/${!valores_lc[$j]}/g" Case_${i}_${valores_lc[$j]}/geometry_script/geometry.geo
+		sed -i "s/\$lccc/${!valores_lc[$j]}/g" "$carpeta_lc/mesh.geo"
+		# sed -i "s/\$lccc/${!valores_lc[$j]}/g" "$carpeta_lc/geometry_script/geometry.geo"
 
-		cd Case_${i}_${valores_lc[$j]}/
-
-		cd "./geometry_script/"
-
-		#Generar mallado gmsh
-		python3 generator_point_process.py
-		./generate.sh
-		cd ..
+		cd "$carpeta_lc/"
+		touch "${valores_lc[$j]}.foam"
 		gmsh "./mesh.geo" -3
-
 		#Genera mallado OpenFoam
 		gmshToFoam "mesh.msh"
 
@@ -122,48 +124,12 @@ for ((i = 1; i <= $cantidad_simulaciones; i++)); do
 		grep -v "physicalType" constant/polyMesh/boundary >constant/polyMesh/boundary.temp
 		mv constant/polyMesh/boundary.temp constant/polyMesh/boundary
 
-		# Reemplaza "patch" por "wall" en las líneas 35
+		# Reemplaza "patch" por "wall" en las líneas 23
 		sed -i '23s/patch/wall/;' "constant/polyMesh/boundary"
 
 		decomposePar
 		mpirun -np 8 simpleFoam -parallel
-
-		reconstructPar
-		foamToVTK
-
-		rm -rR processor*
-
-		mv "constant/" ".."
-		mv "0/" ".."
-		mv "system/" ".."
-		mv "VTK/" ".."
-		mv "mesh.geo" ".."
-
 		cd ..
-
-		rm -rR "Case_${i}_${valores_lc[$j]}/"
-
-		# Crea la carpeta del caso
-		mkdir "Case_${i}_${valores_lc[$j]}"
-
-		mv "constant/" "Case_${i}_${valores_lc[$j]}/"
-		mv "0/" "Case_${i}_${valores_lc[$j]}/"
-		mv "system/" "Case_${i}_${valores_lc[$j]}/"
-		mv "VTK/" "Case_${i}_${valores_lc[$j]}/"
-		mv "mesh.geo" "Case_${i}_${valores_lc[$j]}/"
-
-		# Se copia script de extraccion
-		cp "Case_0/deltap_extract.py" "Case_${i}_${valores_lc[$j]}/"
-
-		# se reemplaza el valor de i y de j+1 en el script de extraccion
-		sed -i "s/\$ii/$i/g" "Case_${i}_${valores_lc[$j]}/deltap_extract.py"
-		sed -i "s/\$jj/$k/g" "Case_${i}_${valores_lc[$j]}/deltap_extract.py"
-
-		cd "Case_${i}_${valores_lc[$j]}/"
-		pvpython deltap_extract.py
-
-		cd ..
-
 	done
 
 	cd ..
